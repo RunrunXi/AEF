4d1d1efbe2e73833657e622acc074c9129f5b4cb
Cyanhana_Neko <111105781+RunrunXi@users.noreply.github.com>
2026-05-30 05:11:34 +0800

    在按键绑定中添加收藏按钮

--- src/main/java/me/cyanhana/aef/Aef.java
+++ src/main/java/me/cyanhana/aef/Aef.java
@@ -1,36 +1,23 @@
 package me.cyanhana.aef;
 
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 
 @Mod(Aef.MODID)
 public class Aef {
     public static final String MODID = "aef";
 
     public Aef(IEventBus modEventBus, ModContainer modContainer) {
-        modEventBus.addListener(this::commonSetup);
-
-        NeoForge.EVENT_BUS.register(this);
-
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event) {
-
-    }
-
-    @EventBusSubscriber
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-
+        if (FMLEnvironment.dist.isClient()) {
+            modEventBus.addListener(this::registerKeyBindings);
         }
     }
+
+    private void registerKeyBindings(final RegisterKeyMappingsEvent event) {
+        ModKeyBindings.register(event);
+    }
+
 }
