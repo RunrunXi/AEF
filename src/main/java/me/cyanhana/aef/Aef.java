d38298f0388e227ed98d370064e6f5b39cab8d9e
Cyanhana_Neko <111105781+RunrunXi@users.noreply.github.com>
2026-06-12 23:21:42 +0800

    构建新1.20.1-Forge项目

--- src/main/java/me/cyanhana/aef/Aef.java
+++ src/main/java/me/cyanhana/aef/Aef.java
@@ -1,44 +1,27 @@
 package me.cyanhana.aef;
 
-import me.cyanhana.aef.client.gui.FavoritesKeys;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.loading.FMLEnvironment;
-import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
-import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
-import net.neoforged.neoforge.common.NeoForge;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Mod(Aef.MODID)
 public class Aef {
     public static final String MODID = "aef";
-    public static final Logger LOGGER = LoggerFactory.getLogger(Aef.class);
+    private static final Logger LOGGER = LogUtils.getLogger();
 
-    public Aef(IEventBus modEventBus, ModContainer modContainer) {
-//        if (FMLEnvironment.dist.isClient()) {
-//            modEventBus.addListener(this::registerKeyBindings);
-//        }
+    public Aef() {
+
     }
 
-//    private void registerKeyBindings(final RegisterKeyMappingsEvent event) {
-//        ModKeyBindings.register(event);
-//    }
-
-    @EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
-        /**
-         * 客户端设置事件处理。
-         * @param event FML客户端设置事件。
-         */
+
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-            FavoritesKeys.init();
+            //TODO: FavoritesKeys.init();
         }
     }
 }
